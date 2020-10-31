f65eda0910b3190108cc1f64878d2ce4a4bb2fcb
root <root@Sam_Asus.localdomain>
2020-10-30 23:50:20 -0700

    test

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pages.urls'))
+    path('', include('pages.urls')) #create a map tp the urls in pages/urls.py
 ,]
--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 #from .views import view_function
-from .views import HomePageView
+from .views import HomePageView # from this folder find the views module and import ____
 from django.conf import settings
 from django.conf.urls.static import static
 
-
+# Takes a list and use path to specify route, function that handles the route
 urlpatterns = [
     # ... the rest of your URLconf goes here ...
     path('', HomePageView.as_view(), name="home"),
